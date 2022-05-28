96198b02c094ed81d8d819cec173a47bf60cd560
Tim Nilsen <nilsentim.e@gmail.com>
2022-05-27 21:50:12 -0700

    finished 9.5.4

--- app.py
+++ app.py
@@ -1,7 +1,50 @@
-from flask import Flask 
+import datetime as dt
+from flask import Flask, jsonify
+from itsdangerous import json
+import numpy as np
+import pandas as pd 
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+engine = create_engine('sqlite:///hawaii.sqlite')
+
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+session = Session(engine)
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World'
+def welcome():
+    return (
+        '''
+        Welcome to the Climate Analysis API!!
+        Available Routes:\n
+        /api/v1.0/precipitation\n
+        /api/v1.0/stations\n
+        /api/v1.0/tobs\n
+        /api/v1.0/temp/start/end\n
+        '''
+    )
+
+@app.route('/api/v1.0/precipitation')    
+def precipitation():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    precipitation = session.query(Measurement.date, Measurement.prcp)\
+        .filter(Measurement.date >= prev_year)\
+            .all()
+    precip = {date: prcp for date, prcp in precipitation}
+    return jsonify(precip)
+
+@app.route('/api/v1.0/stations')
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
